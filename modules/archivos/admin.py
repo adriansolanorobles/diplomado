209181cd9ace7908a72269e47d7ef4521bc0cb9d
adrians <adrian.solano@protonmail.com>
2017-09-27 02:50:02 -0500

    agregue list display admin archivos

--- modules/archivos/admin.py
+++ modules/archivos/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Archivo
 
 class ArchivoAdmin(admin.ModelAdmin):
-	fields = ('user', 'upload', 'upload_calificado')
+	list_display = ('user', 'upload', 'upload_calificado')
+	
 admin.site.register(Archivo, ArchivoAdmin)
