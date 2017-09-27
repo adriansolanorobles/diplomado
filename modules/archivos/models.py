39a73a4bd9afb14138afce1dfa2cb0d7fcce4f2b
adrians <adrian.solano@protonmail.com>
2017-09-27 01:50:30 -0500

    fix archivos etapa

--- modules/archivos/models.py
+++ modules/archivos/models.py
@@ -3,15 +3,12 @@ from modules.users.models import User
 from modules.etapas.models import Etapa
 import time
 
-def content_file_name(instance, filename):
-	    ext = filename.split('.')[-1]
-	    filename = "%s.%s" % (str(int(round(time.time()*1000))), ext)
-	    return os.path.join('archivos/', filename)
+
 # Create your models here.
 class Archivo(models.Model):
     
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    upload = models.FileField(upload_to=content_file_name)
+    upload = models.FileField(upload_to='archivos/')
     etapa = models.ForeignKey(Etapa,on_delete=models.CASCADE)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
