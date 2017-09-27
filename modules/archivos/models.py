7203241ebbfb7b04fe07dc12071aa9dbda0d56ca
adrians <adrian.solano@protonmail.com>
2017-09-27 00:02:02 -0500

    agregue una funcion para renombrar el nombre del archivo

--- modules/archivos/models.py
+++ modules/archivos/models.py
@@ -3,8 +3,13 @@ from modules.users.models import User
 from modules.etapas.models import Etapa
 import time
 
+def content_file_name(instance, filename):
+	    ext = filename.split('.')[-1]
+	    filename = "%s.%s" % (str(int(round(time.time()*1000))), ext)
+	    return os.path.join('archivos/', filename)
 # Create your models here.
 class Archivo(models.Model):
+    
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     upload = models.FileField(upload_to=content_file_name)
     upload_calificado = models.FileField(upload_to=content_file_name, null=True,blank=True)
@@ -13,7 +18,4 @@ class Archivo(models.Model):
     update_time = models.DateTimeField(auto_now=True)
     def __str__(self):
         return str(self.upload) 
-    def content_file_name(instance, filename):
-	    ext = filename.split('.')[-1]
-	    filename = "%s.%s" % (str(int(round(time.time()*1000))), ext)
-	    return os.path.join('archivos/', filename)
+    
