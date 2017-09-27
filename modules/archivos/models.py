a95cb2e5104292bb889caf30af761a85d6b2e325
adrians <adrian.solano@protonmail.com>
2017-09-27 02:33:48 -0500

    agregue upload calificado

--- modules/archivos/models.py
+++ modules/archivos/models.py
@@ -3,13 +3,17 @@ from modules.users.models import User
 from modules.etapas.models import Etapa
 import time
 
-
+def content_file_name(instance, filename):
+        ext = filename.split('.')[-1]
+        filename = "%s.%s" % (str(int(round(time.time()*1000))), ext)
+        return os.path.join('archivos/', filename)
 # Create your models here.
 class Archivo(models.Model):
     
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    upload = models.FileField(upload_to='archivos/')
+    upload = models.FileField(upload_to=content_file_name)
     etapa = models.ForeignKey(Etapa,on_delete=models.CASCADE)
+    upload_calificado = models.FileField(upload_to=content_file_name, null=True,blank=True)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
     def __str__(self):
