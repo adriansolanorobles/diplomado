d70a5c862dc5602408e683a052dad8d693ef7c7d
adrians <adrian.solano@protonmail.com>
2017-09-27 02:45:38 -0500

    agregue campos al admin de archivos

--- modules/archivos/admin.py
+++ modules/archivos/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Archivo
 
 class ArchivoAdmin(admin.ModelAdmin):
-    pass
+	fields = ('user', 'upload', 'upload_calificado')
 admin.site.register(Archivo, ArchivoAdmin)
